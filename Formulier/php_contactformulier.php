c9ac84d85042187f565b7ba98f3b41352befd1d9
SamVerzijde <43599153+SamVerzijde@users.noreply.github.com>
2018-10-29 16:55:02 +0100

    Php
    Php werkt nu.

--- Formulier/php_contactformulier.php
+++ Formulier/php_contactformulier.php
@@ -2,15 +2,16 @@
 if (isset($_POST['submit'])) {
 	$name = $_POST['name'];
 	$mailFrom = $_POST['email'];
-	$phone = $_POST['contact_form'];
-	$subject = $_POST['subject'];
+	$phone = $_POST['contact_phone'];
+	$onderwerp = $_POST['onderwerp'];
 	$comments = $_POST['comments'];
+	$subject = "Contactformulier";
 
 	$mailTo = "sam@pigeonjohn.nl";
 	$headers = "Contactformulier van: ".$mailFrom;
-	$txt = $name, $comments;
+	$txt = $name .$phone .$onderwerp .$comments;
 
-	mail($mailTo, $subject, $txt, $headers);
+	mail($mailTo, $subject, $headers, $txt);
 	header("Location: index.php?mailsend");
 }
 ?>
