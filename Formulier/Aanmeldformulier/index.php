fda6cdba9e63404efaed789110e30e1cf851bd76
SamVerzijde <43599153+SamVerzijde@users.noreply.github.com>
2018-11-07 12:57:39 +0100

    aanmeldformulier

--- Formulier/Aanmeldformulier/index.php
+++ Formulier/Aanmeldformulier/index.php
@@ -1,43 +1,49 @@
 <!DOCTYPE= html>
 <html>
 	<head>
-		<title>Aanmeldformulier</title>
-		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+		<title>
+		</title>
+		<link rel="stylesheet" type="text/css" href="../stylesheet.css">
 	</head>
 	
 	<body>
 		<form id="contactformulier" method="POST" action="php_aanmeldformulier.php">
 			<legend>Aanmelden voor een studie</legend>
-			<ul>
-				<li>Voornamen
+			<label for="contact-surname">
+				Voornamen
+			</label>
 					<input type="text" name="surname" required id="contact-surname"/>
-				</li>
-				<li>Tussenvoegsel
+			<label for="contact-middlename">
+				Tussenvoegsel
 					<input type="text" name="middlename" id="contact-middlename"/>
-				</li>
-				<li>Achternaam
+			</label>
+			<label for="conact-lastname">
+				Achternaam
+			</label>
 					<input type="text" name="lastname" required id="contact-lastname"/>
-				</li>
-				<li>Email
+			<label for="contact-email">
+				Email
+			</label>
 					<input type="email" name="email" required id="contact-email"/>
-				</li>
-				<li>Bevestiging email
+			<label for="contact-email">
+				Bevestiging email
+			</label>
 					<input type="email" name="email" required id="contact-email"/>
-				</li>
-				<li>Telefoonnummer
+			<label for="contact_phone">
+				Telefoonnummer
+			</label>
 					<input type="number" name="contact_phone" maxlength = "10" required id="contact_phone"/>
-				</li>
-				<li>Geboortedatum<em class="msg"></em>
+			<label for="Geboortedatum">
+				Geboortedatum
+			</label>
 					<input type="date" name="Geboortedatum" required id="Geboortedatum"/>
-				</li>
-
-				<li>Ik wil me aanmelden voor:
-					<select id="opleiding" name="opleiding">
-							<option value="Chemie" selected>Chemie</option>
-							<option value="Wiskunde">Wiskunde</option>
-					</select>
-				</li>
-			</ul>
+			<label for="opleding">
+			Ik wil me aanmelden voor:
+			</label>
+				<select id="opleiding" name="opleiding">
+						<option value="Chemie" selected>Chemie</option>
+						<option value="Wiskunde">Wiskunde</option>
+				</select>
 			<input type="submit" name="submit" value="Versturen" />
 			<input type="reset" value="Reset"/>
         </form>
