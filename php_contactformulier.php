364cc906b7cd759489142d0ca6bca9aee55622f3
SamVerzijde <43599153+SamVerzijde@users.noreply.github.com>
2018-11-07 13:19:50 +0100

    contact en aanmeldformulier

---
+++ php_contactformulier.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$mailFrom = $_POST['email'];
+	$phone = $_POST['contact_phone'];
+	$onderwerp = $_POST['onderwerp'];
+	$comments = $_POST['comments'];
+	$subject = "Contactformulier";
+
+	$mailTo = "sam@pigeonjohn.nl";
+	$headers = "Contactformulier van: ".$mailFrom;
+	$txt = $name .$phone .$onderwerp .$comments;
+
+	mail($mailTo, $subject, $headers, $txt);
+	header("Location: index.php?mailsend");
+}
+?>
